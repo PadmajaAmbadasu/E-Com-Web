110c63798dd39295b3ca31b67059b7d720964602
sailasreeanchanala08@gmail.com <sailasreeanchanala08@gmail.com>
2023-08-28 11:50:37 +0530

    first commit

---
+++ src/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const UserModel = new mongoose.Schema({
+  
+  username: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("users", UserModel);
